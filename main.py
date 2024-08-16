bb86b9fe1cc863de84a056b878b84feb510a7156
Elidallana Cristancho <cristanchodayana062017@gmail.com>
2024-08-16 14:49:23 -0500

    Registro de ramas

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from nom_sucursal import nom_sucursal
+from nom_sucursal import registrar_sucursal
+
+def main():
+    while True:
+        print("\nGestion de sucursales")
+        print("1.Registrar sucursal")
--- nom_sucursal.py
+++ nom_sucursal.py
@@ -4,4 +4,5 @@ from datetime import datetime
 registro = {
     "Nombre de la Sucursal"
     "Tefelefono y otros medios de contacto"
+    "Id del gerente o responsable"
 }
